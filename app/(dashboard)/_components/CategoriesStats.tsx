706d5cd8055fa34771606f304492a586b97177a0
yash Patel <125898311+Yash5302@users.noreply.github.com>
2025-06-01 20:08:35 +0530

    CategoriesStats.tsx

--- app/(dashboard)/_components/CategoriesStats.tsx
+++ app/(dashboard)/_components/CategoriesStats.tsx
@@ -81,7 +81,7 @@ function CategoriesCard({
         {filteredData.length === 0 && (
           <div className="flex h-60 w-full flex-col items-center justify-center">
             No data for the selected period
-            <p className="text-sm text-muted-foreground">
+            <p className="text-xs sm:text-sm text-center text-muted-foreground mt-1">
               Try selecting a different period or try adding new{" "}
               {type === "income" ? "incomes" : "expenses"}
             </p>
