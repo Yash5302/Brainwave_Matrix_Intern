fa9631e5019152fb80bc2245fd75a4d57f318c5d
Yash5302 <yjack8954@gmail.com>
2025-06-01 15:47:39 +0530

    inital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import type { Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -6,7 +7,9 @@ import RootProviders from "@/components/providers/RootProviders";
 import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
-
+export const viewport: Viewport = {
+  themeColor: "#2cb05c",
+};
 export const metadata: Metadata = {
   title: "Expense Tracker",
   description: "Expense Tracker Made by Yash Patel",
@@ -18,7 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      layout: {
+        logoImageUrl: "/favicon.ico",
+      },
+      variables: {
+        colorPrimary: "#2cb05c",
+      },
+    }}>
       <html
         lang="en"
         className="dark"
